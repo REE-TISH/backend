1861ce569a19cb5717e229f97f9e3cb1e04a2ab7
REE-TISH <reetishk798@gmail.com>
2025-07-27 19:57:00 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # CORS
 CORS_ALLOWED_ORIGINS = [
-    'https://testing-react-henna.vercel.app',
+    'https://testing-react-henna.vercel.ap',
 ]
 
 # DATABASE (Render will inject DATABASE_URL)
