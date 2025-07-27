7b43a08eee6a8571ae6bdb146dc3952aa5ab7c74
REE-TISH <reetishk798@gmail.com>
2025-07-27 19:59:29 +0530

    Update views.py

--- base/views.py
+++ base/views.py
@@ -7,8 +7,9 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def HomePageData(request):
     return Response({
-        'User':'Reetish',
-        'body':'hello everyone is this workng'
+        'avatar':'https://i.pinimg.com/736x/3e/ce/29/3ece2976d357f517d7da207b2bc0077e.jpg',
+        'User':'Ashley',
+        'body':'Play Resident evil requem'
     })
 
 
