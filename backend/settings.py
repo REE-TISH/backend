e28663633deadee65351b4f62ab2b1a852fc3fb9
REE_TISH <reetishk798@gmail.com>
2025-07-28 12:08:14 +0530

    work please

--- backend/settings.py
+++ backend/settings.py
@@ -63,9 +63,23 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # DATABASE (Render will inject DATABASE_URL)
+
+
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}')
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT", "4000"),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
+    }
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
